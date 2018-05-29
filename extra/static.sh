855e9b44751a5e0ef3bf32ba7eeeadca34b4ca1a
Rahul Sharma <er.csrahul@gmail.com>
2018-05-29 18:04:49 +0530

    Update static.sh

--- extra/static.sh
+++ extra/static.sh
@@ -1,6 +1,6 @@
 bin/magento setup:static-content:deploy -j 1
 if [ $? -eq 0 ]; then
-    curl -X POST --data-urlencode 'payload={"channel": "#gwa-git", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "Static Content Deploy :green_heart: "}' https://hooks.slack.com/services/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
+    curl -X POST --data-urlencode 'payload={"channel": "#webtest", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "Static Content Deploy :green_heart: "}' https://hooks.slack.com/services/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
 else
-    curl -X POST --data-urlencode 'payload={"channel": "#gwa-git", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "Static content deploy need some check :scream: "}' https://hooks.slack.com/services/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
+    curl -X POST --data-urlencode 'payload={"channel": "#webtest", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "Static content deploy need some check :scream: "}' https://hooks.slack.com/services/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
 fi
