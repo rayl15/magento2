7065cc82dcceb36cf3410d652363fe8be8a77794
Rahul Sharma <er.csrahul@gmail.com>
2018-05-30 00:33:57 +0530

    Update checkupgrade.sh

--- extra/checkupgrade.sh
+++ extra/checkupgrade.sh
@@ -3,7 +3,7 @@ bin/magento setup:db:status && UPGRADE_NEEDED=0 || UPGRADE_NEEDED=1
 if [[ 1 == ${UPGRADE_NEEDED} ]]; then
     curl -X POST --data-urlencode 'payload={"channel": "#webtest", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "DB upgrade is required. Upgrading DB :green_heart: "}' https://hooks.slack.com/services/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
   	bin/magento maintenance:enable
-  	sudo bin/magento setup:upgrade --keep-generated
+  	bin/magento setup:upgrade --keep-generated
     bin/magento maintenance:disable 
 else
     curl -X POST --data-urlencode 'payload={"channel": "#webtest", "username": "Deployment '$CODEBUILD_BUILD_ID'", "text": "DB upgrade is *NOT* required :green_heart: "}' https://hooks.slack.com/services/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     
